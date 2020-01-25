e20e9a6b3c3b0dc3f978f19d53df4d29f3bc72dd
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-01-25 16:16:07 +0300

    Arcade library basic implementation with OOP

---
+++ game_dev/arcade/oop_arcade_basics.py
@@ -0,0 +1,23 @@
+import arcade
+
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Welcome to Arcade"
+RADIUS = 150
+
+class Welcome(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_circle_filled(
+            SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
+        )  # implicitly call arcade.finish_render() when .on_draw() ends
+
+
+if __name__ == "__main__":
+    app = Welcome()
+    arcade.run()
