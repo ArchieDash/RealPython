2c6cae07ee38974442f6edd17b5dc931660f6985
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-02-16 12:52:51 +0300

    Text widget

---
+++ GUI/TK/text.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+text_box = tk.Text().pack()
+text = text.box.get("1.0", tk.END)  # strat line index - 1, start char index - 0
+# sing = "\n\nBest regards,\nAlex Nash"
+# text_box.insert(tk.END, sign)
+window.mainloop()
